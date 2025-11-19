e51b964bddf2ea22589cf2b2df18d5c98fdc587e
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2025-11-18 23:05:08 -0300

    product of array except self algorithm

---
+++ productOfArray.ts
@@ -0,0 +1,20 @@
+function productExceptSelf(nums: number[]): number[] {
+  const n = nums.length;
+  const answer = Array(n).fill(0);
+
+  let prefix = 1;
+  for (let i = 0; i < n; i++) {
+    answer[i] = prefix;
+    prefix *= nums[i];
+  }
+
+  let suffix = 1;
+  for (let i = n - 1; i >= 0; i--) {
+    answer[i] *= suffix;
+    suffix *= nums[i];
+  }
+
+  return answer;
+}
+
+productExceptSelf([-1, 1, 0, -3, 3]);
