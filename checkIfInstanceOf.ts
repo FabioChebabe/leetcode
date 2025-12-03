2b4c19bd00934351df09e007d3c5b5aac5ce0d86
Fabio Chebabe Carnelli <fabioccarnelli@gmail.com>
2025-12-03 18:32:27 -0300

    fix: algorithm of check if instance of and improve response time

--- checkIfInstanceOf.ts
+++ checkIfInstanceOf.ts
@@ -1,25 +1,47 @@
+// function checkIfInstanceOf(obj: any, classFunction: any): boolean {
+//   if (
+//     obj === null ||
+//     obj === undefined ||
+//     typeof classFunction !== 'function'
+//   ) {
+//     return false;
+//   }
+
+//   const wrapped = Object(obj);
+
+//   if (
+//     wrapped === null ||
+//     (typeof wrapped !== 'object' && typeof wrapped !== 'function')
+//   ) {
+//     return false;
+//   }
+
+//   let proto = Object.getPrototypeOf(wrapped);
+
+//   while (proto) {
+//     if (proto === classFunction.prototype) return true;
+//     proto = Object.getPrototypeOf(proto);
+//   }
+
+//   return false;
+// }
+
 function checkIfInstanceOf(obj: any, classFunction: any): boolean {
-  if (obj === null || typeof classFunction !== 'function') {
-    return false;
-  }
+  if (obj === null || obj === undefined) return false;
+  if (typeof classFunction !== 'function') return false;
+  if (!classFunction.prototype) return false;
 
   const wrapped = Object(obj);
 
-  if (
-    wrapped === null ||
-    (typeof wrapped !== 'object' && typeof wrapped !== 'function')
-  ) {
-    return false;
-  }
-
   let proto = Object.getPrototypeOf(wrapped);
+  const target = classFunction.prototype;
 
-  while (proto) {
-    if (proto === classFunction.prototype) return true;
+  while (proto !== null) {
+    if (proto === target) return true;
     proto = Object.getPrototypeOf(proto);
   }
 
   return false;
 }
 
-console.log(checkIfInstanceOf(5, Number));
+console.log(checkIfInstanceOf(new Date(), Date));
